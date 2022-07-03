3a7b3b0f4065616d95af65458a6352c777c2f6f2
yutailang0119 <yutailang0119@gmail.com>
2022-07-03 17:15:27 +0900

    Add validation for request input

--- webapp/swift/Sources/IsuConditionOperations/Initialize.swift
+++ webapp/swift/Sources/IsuConditionOperations/Initialize.swift
@@ -19,6 +19,12 @@ import IsuConditionModel
 extension IsuConditionOperationsContext {
     public func handleInitialize(input: IsuConditionModel.InitializeRequestBody) async throws
     -> IsuConditionModel.InitializeAttributes {
+        do {
+            try input.validate()
+        } catch {
+            throw IsuConditionError.badRequestBody(.init(message: "bad request body"))
+        }
+
         let task = Process()
 
         task.arguments = ["-c", "../sql/init.sh"]
@@ -28,7 +34,7 @@ extension IsuConditionOperationsContext {
         do {
             try task.run()
         } catch {
-            throw IsuConditionModel.IsuConditionError.internalServer(.init(message: "exec init.sh error: `\(error)"))
+            throw IsuConditionError.internalServer(.init(message: "exec init.sh error: `\(error)"))
         }
 
         return InitializeAttributes(language: "Swift")
